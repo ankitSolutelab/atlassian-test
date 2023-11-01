80a3a9c650dba9cbc810686d3dc6fc20735e03ec
Ankit Gupta <121420191+ankitSolutelab@users.noreply.github.com>
2023-11-01 15:50:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Hello Jira")
-const credentials = 'ankit.g@solutelabs.com:ATCTT3xFfGN0jkLv8ZJEvjrUs0YSdpvIdO2-gmH6o1zlQniZBqoIBiyKqU3I4lSXHqFOim8ICuqyZZjxnJVJjX3DPhggP2waud9AY7eKFiC42Aj5l43cYZIhUkmM-IZp1z659Q4SI13qhpQ9k4KfdiSxW5q3iCTv24rOCof8vaXNo5-DPvlwsmI=02514DE7';
+const credentials = 'ankit.g@solutelabs.com:ATATT3xFfGF0iUqSOUaaTUGVDWdEjxXCragg-wyoz7Ur2NfPBEsUB3YKX9aH3Mei9dtpQ7tprMQSoIbbsQ5i-YFstpuYZ0zAOUzC6ANdovBOoDa1C-2cMbM3ojMhqizcazV8WJDt1TUXNNCljaSh2AczvG_MNq4yu3q1nlofWwcuXGpBSHE7FJE=E7BB0C08';
 const encodedCredentials = btoa(credentials);
 console.log(encodedCredentials);
 fetch('https://ankitg1.atlassian.net/rest/api/3/project/KYRT', {
