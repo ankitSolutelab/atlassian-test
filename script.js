48a02011f8d955c467f56e49936d4bb307ee62b7
Ankit Gupta <121420191+ankitSolutelab@users.noreply.github.com>
2023-11-01 11:37:06 +0530

    Update 1script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Hello Jira")
 
 fetch('https://ankitg1.atlassian.net/rest/api/3/project/KYRT', {
   method: 'GET',
+  mode: 'no-cors',
   headers: {
     'Authorization': 'Basic ankit.g@solutelabs.com:ATCTT3xFfGN0jkLv8ZJEvjrUs0YSdpvIdO2-gmH6o1zlQniZBqoIBiyKqU3I4lSXHqFOim8ICuqyZZjxnJVJjX3DPhggP2waud9AY7eKFiC42Aj5l43cYZIhUkmM-IZp1z659Q4SI13qhpQ9k4KfdiSxW5q3iCTv24rOCof8vaXNo5-DPvlwsmI=02514DE7',
     'Accept': 'application/json'
